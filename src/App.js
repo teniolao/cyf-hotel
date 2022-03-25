61081a539d3ee0704ab8ef848be6cb1b3639d836
teniolao <Tennybetty100@gmail.com>
2022-03-25 16:57:32 +0000

    complete lesson 1

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import React from "react";
+import Bookings from "./Bookings";
+import "./App.css";
+import Heading from "./Heading";
+import TouristInfoCards from "./TouristInfoCards"; 
+import Footer from "./Footer";
+
+const App = () => {
+  return (
+    <div className="App">
+      <Heading />
+      <TouristInfoCards />
+      <Bookings />
+      <Footer
+        details={[
+          "123 Fake Street, London, E1 4UD",
+          "hello@fakehotel.com",
+          "0123 456789",
+        ]}
+      />
+    </div>
+  );
+};
+
+export default App;
---
+++ src/SearchResults.js
@@ -0,0 +1,56 @@
+import React from "react";
+const moment = require("moment");
+console.log(moment);
+
+const headings = [
+  "id",
+  "title",
+  "firstName",
+  "surname",
+  "email",
+  "roomId",
+  "checkInDate",
+  "checkOutDate",
+  "Duration",
+];
+
+function SearchResults(props) {
+  return (
+    <table className="table">
+      <thead>
+        <tr>
+          {headings.map((heading) => {
+            return <th scope="col">{heading}</th>;
+          })}
+        </tr>
+      </thead>
+      <tbody>
+        {props.bookings.map((booking) => {
+          return (
+            <tr>
+              <th scope="row">{booking.name}</th>
+              {headings.map((heading) => {
+                if (heading === "id") {
+                  return null;
+                } else if (heading === "Duration") {
+                  {
+                    const checkIn = moment(booking.checkInDate);
+                    const checkOut = moment(booking.checkOutDate);
+                    const difference = checkOut.diff(checkIn, "days");
+                    return <td>{difference}</td>
+                  }
+                } else {
+                  return <td>{booking[heading]}</td>;
+                }
+              })}
+              <td>{booking.height}</td>
+              {/* <td>{moment(props.dateOut).diff(moment(props.dateIn), "day")}</td> */}
+            </tr>
+          );
+        })}
+      </tbody>
+    </table>
+  );
+}
+
+export default SearchResults;
